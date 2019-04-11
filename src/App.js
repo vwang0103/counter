8b85ab73b0a587f4ca5b00f405865ad512a1533d
Victor <vwang0103@gmail.com>
2019-04-10 20:45:58 -0400

    Counter works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class HelloWorld extends Component {
-  render() {
+class Counter extends Component {
+  constructor(){
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+  render () {
     return (
-      <div>Hello!</div>
+      <div className= "container">
+      <div className="navbar">Counter.js</div>
+      <div className="counter">
+      <h1>{this.state.count}</h1>
+      <button type="button" onClick={this.increment}> Increment </button>
+      <button type="button"onClick={this.decrement}> Decrement </button>
+      </div>
+    </div>
     );
   }
 }
 
-export default HelloWorld;
+export default Counter;
